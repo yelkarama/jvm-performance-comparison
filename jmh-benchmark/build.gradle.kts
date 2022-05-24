e52c70d1a437ad9a6fb394c0ee3c630e8fc3ccfc
Younes El-karama <yelkarama@users.noreply.github.com>
2022-05-24 02:14:49 -0400

    jmh params

--- jmh-benchmark/build.gradle.kts
+++ jmh-benchmark/build.gradle.kts
@@ -1,11 +1,18 @@
 plugins {
-    id("me.champeau.jmh") version "0.6.6"
+  id("me.champeau.jmh") version "0.6.6"
 }
 
 dependencies {
     implementation(project(":fibonacci"))
 }
 
-tasks.withType<Jar> {
-    duplicatesStrategy = DuplicatesStrategy.INCLUDE
+jmh {
+  fork.set(1)
+  iterations.set(100)
+  timeOnIteration.set("1s")
+  warmupIterations.set(0)
+  benchmarkMode.set( listOf("thrpt") )
+  threads.set(4)
+  includeTests.set(false)
+  duplicateClassesStrategy.set(DuplicatesStrategy.INCLUDE)
 }
