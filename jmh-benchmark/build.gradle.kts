1f679a69dbeca3983004bd0d70f156d94c8874f0
Younes El-karama <yelkarama@users.noreply.github.com>
2022-05-24 02:14:51 -0400

    jmh params

--- jmh-benchmark/build.gradle.kts
+++ jmh-benchmark/build.gradle.kts
@@ -6,4 +6,13 @@ dependencies {
     implementation(project(":fibonacci"))
 }
 
-
+jmh {
+  fork.set(1)
+  iterations.set(100)
+  timeOnIteration.set("1s")
+  warmupIterations.set(0)
+  benchmarkMode.set( listOf("thrpt") )
+  threads.set(4)
+  includeTests.set(false)
+  duplicateClassesStrategy.set(DuplicatesStrategy.INCLUDE)
+}
